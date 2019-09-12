6a3511d9641c0a245d1820b2c457c6aad31174e8
dashen <434857005@qq.com>
2019-09-12 12:14:31 +0800

    加入了一些工具及dll组件

--- mywin32api/mouse.go
+++ mywin32api/mouse.go
@@ -3,6 +3,26 @@
 
 package mywin32api
 
-func Leftlick()  {
+import (
+	"math/rand"
+	"strconv"
+	"time"
+)
+
+func Leftlick() {
 	println("test")
+}
+func SleepStrMs(str_time string) {
+	int_time, _ := strconv.Atoi(str_time)
+	int_time /= 1000
+	time.Sleep(time.Duration(int_time) * time.Second)
+}
+
+func SleepIntS(int_second int) {
+	time.Sleep(time.Duration(int_second) * time.Second)
+}
+func Random(min, max int) int {
+	rand.Seed(time.Now().Unix())
+	randNum := rand.Intn(max-min) + min
+	return randNum
 }
